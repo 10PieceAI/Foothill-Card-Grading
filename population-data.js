6894277bd3b8a7d4849b88c70a8f6dd0ec87218f
10PieceAI <aprophet16@gmail.com>
2025-11-18 19:53:42 -0800

    Create population-data.js with card data
    Added initial population data for card grades.

---
+++ population-data.js
@@ -0,0 +1,15 @@
+const cardPopulationData = {
+    "ABC123": {
+        total: 42,
+        grades: { "Gem Mint 10": 10, "Mint 9": 15, "Near Mint 8": 12, "Excellent 7": 5 }
+    },
+    "XYZ789": {
+        total: 7,
+        grades: { "Gem Mint 10": 3, "Mint 9": 2, "Near Mint 8": 2 }
+    },
+    "LMN456": {
+        total: 120,
+        grades: { "Gem Mint 10": 50, "Mint 9": 40, "Near Mint 8": 20, "Excellent 7": 10 }
+    }
+    // Add more cards here
+};
